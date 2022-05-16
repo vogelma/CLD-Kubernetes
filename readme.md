a0b8e7d4c7542f708f24e01464ebe420ef62d10e
maelle <maelle.vogel@heig-vd.ch>
2022-05-16 17:15:16 +0200

    add deployement don't work

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Aide-mémoire
+
+## Task 2
+
+Changer le contexte pour être sur GKE
+
+kubectl config use-context gke_gke-cluster-1-350109_europe-central2-a_gke-cluster-1
--- startup-config.sh
+++ startup-config.sh
@@ -3,4 +3,5 @@ kubectl create -f redis-pod.yaml
 kubectl create -f api-svc.yaml
 kubectl create -f api-pod.yaml
 kubectl create -f frontend-pod.yaml
-kubectl port-forward frontend 8001:8080
+kubectl create -f frontend-svc.yaml
+#kubectl port-forward frontend 8001:8080
